d1629b111f187688b5c9daeb099b83fc0ae98907
bagavathisingh <bugzx25@gmail.com>
2026-06-25 13:57:02 +0530

    "solved the cors polity error"

--- backend/app/main.py
+++ backend/app/main.py
@@ -40,7 +40,8 @@ origins = [
     "http://localhost:5173",
     "http://localhost:5174",
     "http://127.0.0.1:5173",
-    "http://127.0.0.1:5174"
+    "http://127.0.0.1:5174",
+    "https://formproject-2.onrender.com"
 ]
 
 app.add_middleware(
